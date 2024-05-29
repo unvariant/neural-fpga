86fa48a75c374568e7c783819bc1e2bd5fb4c77c
ABHCI15 <ChalamalasettyA4207@sduhsd.lan>
2024-05-29 11:47:11 -0700

    init memAddr

--- neural.srcs/sources_1/new/access_mem.v
+++ neural.srcs/sources_1/new/access_mem.v
@@ -20,7 +20,14 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module access_mem #( parameter numWeight=5, neuronNo=7
+module access_mem #( parameter numWeight=5, neuronNo=7, actType="ReLU", biasFile="b1_1.mif", weightFile="", widthAddr=11)(
+    input clk,
+    input rst,
+    input wen, // write enable 
+    input ren, // read enable 
+    input[widthAddr-1:0] waddr
+    
 
     );
+    
 endmodule
