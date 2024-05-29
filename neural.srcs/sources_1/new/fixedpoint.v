d9ef9ac51549e12ccae2edc7390add842beed7ca
unvariant <unvariant.winter@gmail.com>
2024-05-29 11:19:40 -0700

    - add fixed point multiply

--- neural.srcs/sources_1/new/fixedpoint.v
+++ neural.srcs/sources_1/new/fixedpoint.v
@@ -1,12 +1,15 @@
-
-
 function [DATAWIDTH-1:0] fmul;
     input [DATAWIDTH-1:0] a;
     input [DATAWIDTH-1:0] b;
-
-    reg [DATAWIDTH*2-1:0] temp;
-
     begin
-
+        if (({'h00000000, a} * {'h00000000, b}) >> (FP_FRACTION * 2) > (1 << FP_INTEGER) - 1) begin
+            fmul <= FP_SATURATING_MAX;
+        end else begin
+            fmul <= ({'h00000000, a} * {'h00000000, b}) >> FP_FRACTION;
+        end
     end
 endfunction
+
+function [DATAWIDTH-1:0] fadd;
+    input [DATAWIDTH-1:0] a;
+    input [DATAWIDTH-1:0] b;
