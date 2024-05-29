94484f40d61cbced328299812f8f8291429c3939
ABHCI15 <ChalamalasettyA4207@sduhsd.lan>
2024-05-29 11:21:23 -0700

    init access_mem.v

---
+++ neural.srcs/sources_1/new/access_mem.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/29/2024 10:55:32 AM
+// Design Name: 
+// Module Name: access_mem
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module access_mem #( parameter numWeight=5, neuronNo=7
+
+    );
+endmodule
